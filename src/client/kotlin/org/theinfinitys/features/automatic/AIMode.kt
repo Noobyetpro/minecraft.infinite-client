2a6cfac60ae01ddafa709297d86141fe6bcbcfc6
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-10-05 00:09:47 +0900

    feat: del unavailabe

--- src/client/kotlin/org/theinfinitys/features/automatic/AIMode.kt
+++ src/client/kotlin/org/theinfinitys/features/automatic/AIMode.kt
@@ -4,13 +4,11 @@ import org.theinfinitys.ConfigurableFeature
 import org.theinfinitys.settings.InfiniteSetting
 
 class AIMode : ConfigurableFeature(initialEnabled = false) {
-    override val settings: List<InfiniteSetting<*>> =
-        listOf(
-            InfiniteSetting.BooleanSetting(
-                "AllowPlayerInput",
-                "AIモード中でもプレイヤーの入力を許可します。",
-                false,
-            ),
-        )
-
+    override val settings: List<InfiniteSetting<*>> = listOf(
+        InfiniteSetting.BooleanSetting(
+            "AllowPlayerInput",
+            "AIモード中でもプレイヤーの入力を許可します。",
+            false,
+        ),
+    )
 }
