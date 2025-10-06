01dd319d62fa8d5889a723e0c8df75fcf7063cc1
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-10-06 09:19:01 +0900

    feat: add feather walk

--- src/client/kotlin/org/theinfinitys/features/Movement.kt
+++ src/client/kotlin/org/theinfinitys/features/Movement.kt
@@ -4,40 +4,35 @@ import org.theinfinitys.feature
 import org.theinfinitys.features.movement.AutoMine
 import org.theinfinitys.features.movement.AutoWalk
 import org.theinfinitys.features.movement.FastBreak
+import org.theinfinitys.features.movement.FeatherWalk
 import org.theinfinitys.features.movement.Freeze
 import org.theinfinitys.features.movement.SafeWalk
 import org.theinfinitys.features.movement.SuperSprint
 
-val movement =
-    listOf(
-        feature(
-            "SuperSprint",
-            SuperSprint(),
-            "スプリントを拡張します",
-        ),
-        feature(
-            "SafeWalk",
-            SafeWalk(),
-            "地形の橋から落下せずに安全に移動できるようになります",
-        ),
-        feature(
-            "Freeze",
-            Freeze(),
-            "有効にしている間は、サーバーにデータを送信しません",
-        ),
-        feature(
-            "AutoWalk",
-            AutoWalk(),
-            "自動で前進します",
-        ),
-        feature(
-            "AutoMine",
-            AutoMine(),
-            "自動でブロックを採掘します",
-        ),
-        feature(
-            "FastBreak",
-            FastBreak(),
-            "ブロックを壊す際のインターバルを削除します",
-        ),
-    )
+val movement = listOf(
+    feature(
+        "SuperSprint",
+        SuperSprint(),
+        "スプリントを拡張します",
+    ), feature(
+        "SafeWalk",
+        SafeWalk(),
+        "地形の橋から落下せずに安全に移動できるようになります",
+    ), feature(
+        "Freeze",
+        Freeze(),
+        "有効にしている間は、サーバーにデータを送信しません",
+    ), feature(
+        "AutoWalk",
+        AutoWalk(),
+        "自動で前進します",
+    ), feature(
+        "AutoMine",
+        AutoMine(),
+        "自動でブロックを採掘します",
+    ), feature(
+        "FastBreak",
+        FastBreak(),
+        "ブロックを壊す際のインターバルを削除します",
+    ), feature("FeatherWalk", FeatherWalk(), "畑などで走ったりしないようにします。")
+)
